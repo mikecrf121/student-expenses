c4b1aeb4568fc4410f5bd5223f55e7d03c7e0f8a
mikecrf121 <cassm.mc@gmail.com>
2020-12-15 12:25:32 -0500

    added utils service to be used later

---
+++ client/src/app/_services/util.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+/**
+ *   Use this later for global functions needed
+ *   for calculate disbursements could go here..
+ *
+ */
+@Injectable({
+  providedIn: 'root'
+})
+export class UtilService {
+
+  constructor() { }
+}
