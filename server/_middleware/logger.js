5fd8aca8375be535031b8d39fd2c1bd454519bdd
mikecrf121 <cassm.mc@gmail.com>
2020-12-14 21:53:52 -0500

    mc

--- server/_middleware/logger.js
+++ server/_middleware/logger.js
@@ -1,5 +1,6 @@
 const moment = require("moment");
 // remove this from app.use in main server file for performance boost
+// TODO log response times
 const logger = (req, res, next) => {
   console.log(
     `${req.method}-${req.protocol}://${req.get("host")}${
